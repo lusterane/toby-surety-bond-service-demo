82ee44736b55bcfe34b15ccb64e211fb36c8e3ba
Toby <tobychow98@gmail.com>
2026-03-09 22:05:12 -0500

    add dashboard to appshell outlet

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 
 export default function AppShell() {
   return (
-    <>
+    <div className="app-shell">
       <Sidebar />
-    </>
+      <main className="app-main">
+        <Outlet />
+      </main>
+    </div>
   );
 }
