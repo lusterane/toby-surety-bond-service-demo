676bb207d7a767aa2488313e13f0f3c4b1c7c8aa
Toby <tobychow98@gmail.com>
2026-03-09 22:37:21 -0500

    added second mock

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -16,4 +16,24 @@ export const initialBonds: Bond[] = [
     obligee: { id: "1", name: "Company LLC", address: "456 St, Plano TX, USA" },
     status: "Active",
   },
+  {
+    id: "SB-2026-002",
+    bondAmount: 200000,
+    premium: 2000,
+    effectiveDate: new Date(),
+    expirationDate: new Date(),
+    principal: {
+      id: "2",
+      name: "Mike",
+      address: "789 St, Plano TX, USA",
+      creditScore: 700,
+      yearsInBusiness: 10,
+    },
+    obligee: {
+      id: "2",
+      name: "Big Corp LLC",
+      address: "101 St, Plano TX, USA",
+    },
+    status: "Active",
+  },
 ];
